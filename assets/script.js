87dd7ab55d7c04c9873e32f81b15e3cef49ca80c
flancast90 <flancast90@gmail.com>
2022-08-27 13:12:16 -0400

    add file rendering, templating, minor UI changes

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,16 @@
-async function getGithubFiles(user="flancast90", name="demo.finnsoftware.net") {
+function parseUrl() {
+    const url = new URLSearchParams(window.location.search);
+    return url.get('dir');
+}
+
+async function addHeader(elem=document.getElementById("header")) {
+    var header = await fetch("components/header.component");
+    var headerCode = await header.text(); 
+
+    elem.innerHTML = headerCode;
+}
+
+async function getGithubFiles(user="flancast90", name="github-demos") {
     const response = await fetch(`https://api.github.com/repos/${user}/${name}/contents`);
     const data = await response.json();
     return data;
@@ -19,7 +31,7 @@ function renderDirs(dirs, elem=document.getElementById("demos")) {
     elem.innerHTML = "";
     for (const dir of dirs) {
         elem.innerHTML += `
-        <div onclick="window.open('show-files?dir=${dir.name}', '_self')" class="cursor-pointer shadow-lg rounded-lg bg-secondary-bg-color p-3 w-1/4 text-sm hover:bg-bg-hover-color">
+        <div onclick="window.open('show-files.html?dir=${dir.name}', '_self')" class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 cursor-pointer shadow-lg rounded-lg bg-secondary-bg-color p-3 text-sm hover:bg-bg-hover-color">
             <p class="flex flex-col items-center justify-center text-text-secondary font-semibold">
                 <svg class="w-1/2 text-blue-accent-muted" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 490 490" fill="currentColor">
                     <g>
@@ -36,4 +48,46 @@ function renderDirs(dirs, elem=document.getElementById("demos")) {
         </div>
         `
     }
+}
+
+async function getFilesFromDir(dir) {
+    const response = await fetch(`https://api.github.com/repos/flancast90/github-demos/contents/${dir}`);
+    const data = await response.json();
+    
+    return data;
+}
+
+function renderFile(dirName, files, elem=document.getElementById("demos")) {
+    if (files.length === 0) return;
+
+    elem.innerHTML = "";
+    for (const file of files) {
+        if (file.type === "file") {
+            elem.innerHTML += `
+            <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 cursor-pointer shadow-lg rounded-lg bg-secondary-bg-color p-3 text-sm hover:bg-bg-hover-color">
+                <p class="flex flex-col items-center justify-center text-text-secondary font-semibold">
+                    <p class="text-lg">${file.name}</p>
+                    <iframe class="w-full" src="${dirName}/${file.name}" frameborder="0" class="pointer-events-none"></iframe>
+                    <a href="${file.html_url}" class="text-sm">View Code</a><br>
+                    <a href="${dirName}/${file.name}" class="text-sm">View Live</a>
+                </p>
+            </div>
+            `
+        }
+    }
+}
+
+window.onload = async function() {
+    await addHeader();
+    const repos = await getGithubFiles();
+    const dirs = getAllDirs(repos);
+    const dirName = parseUrl()
+    
+    if (dirName) {
+        const files = await getFilesFromDir(dirName);
+        renderFile(dirName, files);
+        return;
+    }
+    
+    renderDirs(dirs);
 }
