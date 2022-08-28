160ab8c51b974f217ce56923bc41cd9718c8cd89
Finn Lancaster <flancast90@gmail.com>
2022-08-28 09:58:32 -0400

    fix change from dev branch merge

--- assets/script.js
+++ assets/script.js
@@ -12,7 +12,7 @@ async function addHeader(elem=document.getElementById("header")) {
     elem.innerHTML = headerCode;
 }
 
-async function getGithubFiles(user=userInformation.gh_name, name=userInformation.gh_repo, branch="flancast90") {
+async function getGithubFiles(user=userInformation.gh_name, name=userInformation.gh_repo, branch="main") {
     const response = await fetch(`https://api.github.com/repos/${user}/${name}/contents/?ref=${branch}`);
     const data = await response.json();
     return data;
@@ -56,7 +56,7 @@ function renderDirs(dirs, elem=document.getElementById("demos")) {
     }
 }
 
-async function getFilesFromDir(dir, branch="flancast90", user=userInformation.gh_name, name=userInformation.gh_repo) {
+async function getFilesFromDir(dir, branch="main", user=userInformation.gh_name, name=userInformation.gh_repo) {
     const response = await fetch(`https://api.github.com/repos/${user}/${name}/contents/${dir}?ref=${branch}`);
     const data = await response.json();
     
