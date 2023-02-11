69d38e81c8b2f3d1bae92a48cee12ddbe1934481
Finn Lancaster <flancast90@gmail.com>
2023-02-11 10:42:28 -0500

    Add commas to number and IntersectionObserver

--- angels-for-change/local-assets/script.js
+++ angels-for-change/local-assets/script.js
@@ -1,20 +1,31 @@
 const controls = {
-    speed: 200,
-    elems: document.querySelectorAll('.counter'),
-}
+  speed: 200,
+  elems: document.querySelectorAll('.counter'),
+};
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      const counter = entry.target;
+      const target = parseInt(counter.getAttribute('data-countto'));
+      const count = numeral(counter.innerText).value();
+      const increment = Math.trunc(target / controls.speed);
+
+      const updateCount = () => {
+        if (count < target) {
+          counter.innerText = numeral(count + increment).format('0, 0');
+          setTimeout(updateCount, 1);
+        } else {
+          counter.innerText = target;
+        }
+      };
+      updateCount();
+
+      observer.unobserve(counter);
+    }
+  });
+});
 
 controls.elems.forEach((counter) => {
-  const updateCount = () => {
-    const target = parseInt(counter.getAttribute('data-countto'));
-    const count = parseInt(counter.innerText);
-    const increment = Math.trunc(target / controls.speed);
-
-    if (count < target) {
-      counter.innerText = count + increment;
-      setTimeout(updateCount, 1);
-    } else {
-      counter.innerText = target;
-    }
-  };
-  updateCount();
+  observer.observe(counter);
 });
